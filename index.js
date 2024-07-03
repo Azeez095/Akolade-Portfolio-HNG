42ec78d3f8964de87825e7ceb6540ea3785909a3
AMOO AKOLADE <akoladeamoo95@gmail.com>
2024-07-03 16:32:53 +0100

    updated

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     function updateTime() {
         const now = new Date();
-        const utcTime = now.toUTCString().slice(-12, -4);
+        const utcTime = new Date(now.getTime() + 3600000);
+        const timeString = utcTime.toUTCString().slice(-12, -4);
         const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        const currentDay = days[now.getUTCDay()];
+        const currentDay = days[utcTime.getUTCDay()];
 
-        document.getElementById('utc-time').textContent = utcTime;
+        document.getElementById('utc-time').textContent = timeString;
         document.getElementById('current-day').textContent = currentDay;
-        }
+    }
 
     updateTime();
     setInterval(updateTime, 1000);
